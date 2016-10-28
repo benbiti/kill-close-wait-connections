052ab65da7603faa924bb51b76b56846c655e0d2
benbiti <wangshouben@hotmail.com>
2016-10-28 10:00:11 +0800

    clear ffff in ip address

--- kill_close_wait.sh
+++ kill_close_wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-netstat -tulnap | grep CLOSE_WAIT | awk '{print $4,$5}' |  sed 's/:/ /g' | while read line; do
-	perl /opt/scripts/killcw.pl $line;
+netstat -tulnap | grep CLOSE_WAIT | awk '{print $4,$5}' |  sed 's/:/ /g' | sed 's/ffff//g' | while read line; do
+    perl ./kill_close_wait_connections.pl $line
 done
 
